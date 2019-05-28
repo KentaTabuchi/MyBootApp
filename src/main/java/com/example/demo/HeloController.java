14401f46c388237002d6cdb6447438dcf1e965a0
田淵健太 <>
2019-05-29 07:37:35 +0900

    This is Tutorial of implemets a form.

--- src/main/java/com/example/demo/HeloController.java
+++ src/main/java/com/example/demo/HeloController.java
@@ -1,14 +1,27 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class HeloController {
 
-	@RequestMapping("/")
-		public String index() {
-		return "Hello World!!";
+	@RequestMapping(value="/",method=RequestMethod.GET)
+		public ModelAndView index(ModelAndView mav) {
+		mav.setViewName("index");
+		mav.addObject("msg","お名前を書いて送信してください");
+		return mav;
+	}
+	
+	@RequestMapping(value="/",method=RequestMethod.POST)
+	public ModelAndView send(@RequestParam("text1")String str,ModelAndView mav) {
+		mav.addObject("msg","こんにちは"+str+"さん");
+		mav.addObject("value",str);
+		mav.setViewName("index");
+		return mav;
 	}
 	
 }
