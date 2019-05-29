746ff33b6620eb412876a3373118a4c357348a98
田淵健太 <>
2019-05-29 20:40:03 +0900

    I comfirmed how to send variable to html's table.and how can use foreach by thymeleaf.

--- src/main/java/com/example/demo/HeloController.java
+++ src/main/java/com/example/demo/HeloController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +14,12 @@ public class HeloController {
 	@RequestMapping(value="/",method=RequestMethod.GET)
 		public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("index");
+		ArrayList<DataObject> data = new ArrayList<DataObject>();
+		data.add(new DataObject(0,"taro","090-1234,5678"));
+		data.add(new DataObject(1,"hanako","090-9876,5432"));
+		data.add(new DataObject(2,"sachiko","090-2345,8765"));
+
+		mav.addObject("data", data);
 		mav.addObject("msg","お名前を書いて送信してください");
 		return mav;
 	}
